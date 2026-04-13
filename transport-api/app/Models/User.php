19571670215d8cdc8a6af5ddf1d7a4b3db116c41
abderrahmane140 <bsarabderrahmane01@gmail.com>
2026-04-13 10:26:03 +0100

    add hasapitokens to user model

--- transport-api/app/Models/User.php
+++ transport-api/app/Models/User.php
@@ -7,13 +7,14 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 use PHPUnit\Framework\MockObject\NoMoreReturnValuesConfiguredException;
 use PHPUnit\Framework\MockObject\Stub\ReturnReference;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
