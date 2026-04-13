6705125a22fb4b9c9b7d07a1a967bf7a8f8bae22
abderrahmane140 <bsarabderrahmane01@gmail.com>
2026-04-13 10:27:51 +0100

    add the missing  namespace on the auth controller

--- transport-api/app/Http/Controllers/Api/AuthController.php
+++ transport-api/app/Http/Controllers/Api/AuthController.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 
-class AuthController extends Controller
+class AuthController  extends Controller
 {
 
     /**
