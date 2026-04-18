290eca4f368c0bce4517749c817ed68306657062
abderrahmane140 <bsarabderrahmane01@gmail.com>
2026-04-19 00:08:55 +0100

    fix the bug naming on tripstore

--- transport-frontend/src/store/tripStore.js
+++ transport-frontend/src/store/tripStore.js
@@ -1,32 +1,32 @@
-const userTripStore = create((set, get) =>({
-    activeTrips:     [],
+import { create } from 'zustand'
+
+const useTripStore = create((set, get) => ({
+  activeTrips:     [],
+  currentTrip:     null,
+  vehiclePosition: null,
+  eta:             null,
+  locationHistory: [],
+
+  setActiveTrips:      (trips)    => set({ activeTrips: trips }),
+  setCurrentTrip:      (trip)     => set({ currentTrip: trip }),
+  setVehiclePosition:  (position) => set({ vehiclePosition: position }),
+  setEta:              (eta)      => set({ eta }),
+
+  addLocationToHistory: (loc) =>
+    set((s) => ({ locationHistory: [...s.locationHistory.slice(-300), loc] })),
+
+  // Called by Echo WebSocket listener on every location update
+  onLocationUpdated: (data) => {
+    set({ vehiclePosition: data.location })
+    get().addLocationToHistory(data.location)
+  },
+
+  reset: () => set({
     currentTrip:     null,
     vehiclePosition: null,
     eta:             null,
     locationHistory: [],
-
-
-    setActiveTrips:      (trips)    => set({activeTrips: trips}),
-    setCurrentTrip:      (trip)     => set({ currentTrip: trip }),
-    setVehiclePosition:  (position) => set({ vehiclePosition: position }),
-    setEta:              (eta)      => set({ eta }),
-
-    addLocationToHistory: (loc) =>
-        set((s) => ({locationHistory: [...s.locationHistory.slice(-300), loc]})),
-
-    //Called by Echo Websocket listener on every location update 
-    onLocationUpdated: (data) => {
-        set({vehiclePosition: data.location})
-        get().addLocationToHistory(data.location)
-    },
-
-    reset: () => set({
-        currentTrip:  null,
-        vehiclePosition:  null,
-        eta             : null,
-        locationHistory: []
-    })
+  }),
 }))
 
-
-export default userTripStore
+export default useTripStore
